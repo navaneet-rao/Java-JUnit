053194203485b90a643be6997be36cdf99093766
Navaneet r rao <76439641+navaneet-rao@users.noreply.github.com>
2024-01-27 14:32:32 +0530

    second commit from eclipse - annotate type

--- src/test/java/org/navaneet/MathUtilsTest.java
+++ src/test/java/org/navaneet/MathUtilsTest.java
@@ -2,25 +2,49 @@ package org.navaneet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
 
+	MathUtils mathUtiles;
+
+	@BeforeAll
+	static void beforeAllInit() {
+		System.out.println("this needs to run before all");
+	}
+	
+	@BeforeEach
+	void init() {
+		mathUtiles = new MathUtils();
+	}
+
+	@AfterEach
+	void cleanUp() {
+		System.out.println("Cleaning up....");
+	}
+
 	@Test
 	void testAdd() {
 //		System.out.println("This test ran");
-		MathUtils mathUtiles = new MathUtils();
+//		MathUtils mathUtiles = new MathUtils();
 		int expected = 2;
 		int actual = mathUtiles.add(1, 1); // change (1, 2) for test error
 		assertEquals(expected, actual, "The Add method should add 2 numbers");
-		
+	}
+
+	@Test
+	void testDiv() {
+//		MathUtils mathUtiles = new MathUtils();
+		assertThrows(ArithmeticException.class, () -> mathUtiles.div(1, 0), "divid by zero should throw error");
 	}
 
 	@Test
 	void testComputeCircleArea() {
-		MathUtils mathUtils = new MathUtils();
-		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "should return the area of circle");
-		
+//		MathUtils mathUtils = new MathUtils();
+		assertEquals(314.1592653589793, mathUtiles.computeCircleArea(10), "should return the area of circle");
 	}
-	
+
 }

2f74bc738b372ae35d28c97c2f83de77dbf67168
Navaneet r rao <76439641+navaneet-rao@users.noreply.github.com>
2024-01-27 23:43:27 +0530

    last commit from eclipse

--- src/test/java/org/navaneet/MathUtilsTest.java
+++ src/test/java/org/navaneet/MathUtilsTest.java
@@ -9,7 +9,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestReporter;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 //import org.junit.jupiter.api.TestInstance;
@@ -19,6 +24,8 @@ import org.junit.jupiter.api.condition.OS;
 class MathUtilsTest {
 
 	MathUtils mathUtiles;
+	TestInfo testInfo;
+	TestReporter testReporter;
 
 	@BeforeAll
 	static void beforeAllInit() {
@@ -26,7 +33,11 @@ class MathUtilsTest {
 	}
 
 	@BeforeEach
-	void init() {
+	void init(TestInfo testInfo, TestReporter testReporter) {
+		this.testInfo = testInfo;
+		this.testReporter = testReporter;
+		System.out.println("");
+		testReporter.publishEntry("Running " + testInfo.getDisplayName() + " With Tags " + testInfo.getTags());
 		mathUtiles = new MathUtils();
 	}
 
@@ -47,6 +58,7 @@ class MathUtilsTest {
 
 	@Nested
 	@DisplayName("Add Method")
+	@Tag("Math")
 	class AddTest {
 		@Test
 		@DisplayName("When adding two positive numbers")
@@ -62,7 +74,18 @@ class MathUtilsTest {
 	}
 
 	@Test
+	@DisplayName("multiplation method")
+	@Tag("Math")
+	void testMulti() {
+//		assertEquals(4, mathUtiles.mul(2, 2), "should return the right product");
+		assertAll(() -> assertEquals(4, mathUtiles.mul(2, 2)), () -> assertEquals(0, mathUtiles.mul(2, 0)),
+				() -> assertEquals(-2, mathUtiles.mul(2, -1)));
+	}
+
+	@Test
 //	@EnabledOnOs(OS.LINUX)
+	@EnabledOnOs(OS.WINDOWS)
+	@Tag("Math")
 	void testDiv() {
 //		MathUtils mathUtiles = new MathUtils();
 		boolean isServerUp = false;
@@ -70,15 +93,18 @@ class MathUtilsTest {
 		assertThrows(ArithmeticException.class, () -> mathUtiles.div(1, 0), "divid by zero should throw error");
 	}
 
-	@Test
-	void testComputeCircleArea() {
+//	@Test
+	@RepeatedTest(3)
+	void testComputeCircleArea(RepetitionInfo repetitionInfo) {
 //		MathUtils mathUtils = new MathUtils();
+//		repetitionInfo.
 		assertEquals(314.1592653589793, mathUtiles.computeCircleArea(10), "should return the area of circle");
 	}
 
 	@Test
 	@Disabled
 	@DisplayName("This method should not run")
+	@Tag("Circle")
 	void testDisabled() {
 		fail("This test should be disabled");
 	}
